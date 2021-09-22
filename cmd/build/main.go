34060925b8681359c345895c49c8871b48ffc844
forewing <jujianai@hotmail.com>
2021-09-22 17:29:56 -0400

    Add build tool

---
+++ cmd/build/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/forewing/gobuild"
+)
+
+const (
+	name = "csgo-rcon"
+)
+
+var (
+	flagAll = flag.Bool("all", false, "build for all platforms")
+
+	target = gobuild.Target{
+		Source:      "./cmd/csgo-rcon",
+		OutputName:  name,
+		OutputPath:  "./output",
+		CleanOutput: true,
+
+		ExtraFlags:   []string{"-trimpath"},
+		ExtraLdFlags: "-s -w",
+
+		VersionPath: "",
+		HashPath:    "",
+
+		Compress:  gobuild.CompressRaw,
+		Platforms: []gobuild.Platform{{}},
+	}
+)
+
+func main() {
+	flag.Parse()
+	if *flagAll {
+		target.OutputName = fmt.Sprintf("%s-%s-%s-%s",
+			name,
+			gobuild.PlaceholderVersion,
+			gobuild.PlaceholderOS,
+			gobuild.PlaceholderArch)
+		target.Compress = gobuild.CompressAuto
+		target.Platforms = gobuild.PlatformCommon
+	}
+	err := target.Build()
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/chzyer/logex v1.1.10 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
 	github.com/chzyer/test v0.0.0-20210722231415-061457976a23 // indirect
+	github.com/forewing/gobuild v0.4.0
 	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
 )
